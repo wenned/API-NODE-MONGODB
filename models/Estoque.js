03f6f17f7a2ae5ea04416a41566ba4bf0911dfdb
wenned <wenned@gmail.com>
2023-06-19 17:21:53 -0300

    refatoramento de toda estrutura

--- models/Estoque.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const EstoqueSchema = mongoose.model('Estoque',
-	{
-	"Carne":Number,
-	"Id":String
-})
-module.exports = mongoose.model('Modelo', EstoqueSchema)
