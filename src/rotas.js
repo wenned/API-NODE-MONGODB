601a0a057b8161f8b7e81e960dea64b8eccf99a5
wenned <wenned@gmail.com>
2023-06-21 14:14:52 -0300

    Limpeza

--- src/rotas.js
+++ src/rotas.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import home from './Home/home.js'
 import inserirPedido from './ModulosInternos/inserirPedido.js'
 import getInformacao from './ModulosInternos/informacao.js'
-import entradaSaida from './ModulosInternos/entradaSaida.js'
 import alteracaoPedido from './ModulosInternos/alteracaoInformacoes.js'
 import {fecharCaixa, conferirCaixa} from './ModulosInternos/fechamentoCaixa.js'
 
@@ -10,11 +9,11 @@ const rotas = Router();
 
 //Rotas de I/O
 rotas.post('/inserir', inserirPedido) //FASE TEST
-rotas.put('/mesa/:Mesa?/:Id?', entradaSaida) //EM DESENVOLVIMENTO
 rotas.put('/entrada/:Funcao?', alteracaoPedido) //FASE DE INTEGRACAO COM FONT-END
 
 // Rotas de Fechamento de Caixa
 rotas.post('/fechamento_caixa', fecharCaixa) // EM DESENVOLVIMENTO
+rotas.put('/fechamento_caixa', conferirCaixa) // EM DESENVOLVIMENTO
 
 // Rotas de Informacoes
 rotas.get('/', home) // FASE TESTE
