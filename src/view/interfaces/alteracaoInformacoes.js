c4435443dbf153690c1fa65c92ab3d66b7024772
wenned <wenned@gmail.com>
2024-02-15 23:36:45 -0300

    criacao interface de resposta aos endpoints

--- src/view/interfaces/alteracaoInformacoes.js
+++ src/view/interfaces/alteracaoInformacoes.js
@@ -1,9 +1,40 @@
 import AlteracaoProduct from '../../controllers/alterarProduct.js'
 
-const product = AlteracaoProduct;
+const product = new AlteracaoProduct();
 
 
 async function alteracaoPedido (req, res) {
+	
+	const r  = req.body
+	
+	switch(req.params.Funcao){
+	
+		case 'liberaMesa':
+		case 'ocuparMesa':
+		case 'alteraMesa':
+			const libera = await product.setAlterarStatusMesa(r.operacao, r.id)
+			res.status(201).json(libera)
+			break
+	
+		case 'removerItemPedido':
+			const remover = await product.removerItemPedido(r.id, r.index);
+			res.status(201).json(remover)
+			break
+
+		case 'pedidoFeito':
+			const pedido = await product.setPedidoFeito(r.first_id, r.second_id);
+			res.status(201).json(pedido);
+			break
+
+		case 'inserirItemPedido':
+			const inserir = await product.setInserirItemPedido(r);
+			res.status(201).json(inserir)
+			break
+
+		default:
+			break
+
+	}
 
 };
 
