839d1d4ecb0fbbb11cd4a91c001e1f90ac4d8c2c
wenned <wenned@gmail.com>
2024-06-04 21:30:56 -0300

    criacao metodo de insersao de adicional no pedido, como remocao do item no estoque

--- src/controllers/alterarEstoque.js
+++ src/controllers/alterarEstoque.js
@@ -1,16 +1,29 @@
 import Estoque from '../models/Estoque.js'
 
 async function AlteraStoque(...args){
-	for(var index=0; index < args[0].length; index++){
 
+	for(var index=0; index < args[0].length; index++){
+	
 		const sabor =  	args[0][index]['Item']['Sabor']
 
 		for(var item=0; item < sabor.length; item++){
 			const ItemEstoque = await Estoque.findOne({Tipo:sabor[item]});
 			const newQuantidade = ItemEstoque.Quantidade - args[0][index]['Item']['Quantidade']
 			await Estoque.updateOne({Tipo:sabor[item]}, {Quantidade:newQuantidade})
-		}
-	}
+		};
+	};
+	
+	for(var i = 0; i < args[0].length; i++){
+		
+		const item = args[0][i]['Item']['Adicional'];
+			
+		for(var it = 0; it < item.length; it++){
+			const take = await Estoque.findOne({Tipo:item[it]});
+			const newValue = take.Quantidade - 1
+			await Estoque.updateOne({Tipo:item[it]}, {Quantidade:newValue})
+		};
+	};
+
 };
 
 async function retornaEstoque(...args){
@@ -25,6 +38,18 @@ async function retornaEstoque(...args){
 			await Estoque.updateOne({Tipo:sabor[Item]}, {Quantidade:newQuantidade})
 		}
 	}
+/*	
+	for(var i = 0; i < args[0].length; i++){
+		
+		const item = args[0][i]['Item']['Adicional'];
+			
+		for(var it = 0; it < item.length; it++){
+			const take = await Estoque.findOne({Tipo:item[it]});
+			const newValue = take.Quantidade + 1
+			await Estoque.updateOne({Tipo:item[it]}, {Quantidade:newValue})
+		};
+	};
+*/
 };
 
 
