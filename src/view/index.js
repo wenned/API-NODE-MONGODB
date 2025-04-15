60518f84413561331020a421cd46ee0bb416533b
wenned <wenned@gmail.com>
2025-04-15 09:42:32 -0300

    importacao e carregamento env

--- src/view/index.js
+++ src/view/index.js
@@ -1,51 +1,47 @@
-// Configuracoes Express
+import '../config/env.js';
 
+// Configuracoes Express
 import express from 'express';
 import session from 'express-session';
 
 // Configuracoes cross-Origin
-
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 // Configuracaes conexao com banco de dados e rotas de acesso.
+import connectDatabase from '../conectMongo.js';
+import rotas from '../view/rotas.js';
 
-	import connectDatabase from '../conectMongo.js';
-	import rotas from '../view/rotas.js';
+const app = express();
 
-	const app = express();
+app.use(cookieParser());
 
-	app.use(cookieParser());
-
-	app.use(session({
+app.use(session({
 		
-		secret:'GEP',
-		resave: false,
-		saveUninitialized: true
+	secret:'GEP',
+	resave: false,
+	saveUninitialized: true
+}));
 
-	}));
-
-	app.use((req, res, next)=>{
+app.use((req, res, next)=>{
 		
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, PATCH, OPTIONS");
-		res.header("Access-Control-Allow-Headers", "Content-Type");
-		app.use(cors());
-		next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, PATCH, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	app.use(cors());
+	next();
+});
 
-	})
+app.use(express.json());
+app.use(rotas)
 
-	app.use(express.json());
+//Inicinando servidor
 
-	app.use(rotas)
-
-	//Inicinando servidor
-
-	connectDatabase()
-		.then(()=>{	
-			app.listen(8080, ()=>{
-				console.log('Servidor iniciado na porta 8080')
-			});
-		}).catch((err)=>{
-			console.log('Erro ao Connectar a Base de Dados', err)
+connectDatabase()
+	.then(()=>{	
+		app.listen(8080, ()=>{
+			console.log('Servidor iniciado na porta 8080')
 		});
+	}).catch((err)=>{
+		console.log('Erro ao Connectar a Base de Dados', err)
+	});
