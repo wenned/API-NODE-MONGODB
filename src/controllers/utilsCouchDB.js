1d3ef6ab70e1f086259e7f845f9e5f47c910333d
wenned <wenned@gmail.com>
2025-04-24 09:49:25 -0300

    Estruturacao de modulo

--- src/controllers/utilsCouchDB.js
+++ src/controllers/utilsCouchDB.js
@@ -1,36 +1,42 @@
 import { config } from '../config/env.js'
 
-export async function alterarNumeroPedidoCouchDB (...args){
+export default class  CouchdbUtils { 
 	
-	const auth = btoa(`${config.userId}:${config.userKey}`)
+	async alterarNumeroPedidoCouchDB (...args){
+	
+		const auth = btoa(`${config.userId}:${config.userKey}`)
 
-	try{
-        //consultando documento
-		const response = await fetch(`${config.couchdbUrl}/nupedidos/_all_docs?include_docs=true`,
-			{
-				method: 'GET',
-				headers:{
-					'Authorization': `Basic ${auth}`
-				},
-				credentials:'include'
-			});
+		try{
+			//consultando documento
+			const response = await fetch(`${config.couchdbUrl}/nupedidos/_all_docs?include_docs=true`,
+				{
+					method: 'GET',
+					headers:{
+						'Authorization': `Basic ${auth}`
+					},
+					credentials:'include'
+				});
 
-		const data = await response.json();
-		data.rows[0].doc.Nu_pedido = args[0]
+			const data = await response.json();
+			data.rows[0].doc.Nu_pedido = args[0]
 		
-		//salvando no banco
+			//salvando no banco
 
-		const persit = await fetch(`${config.couchdbUrl}/nupedidos/${data.rows[0].doc._id}`,
-			{
-				method: 'PUT',
-				headers:{
-					'Authorization': `Basic ${auth}`,
-					'Content-type': 'application/json',
-				},
-				body: JSON.stringify(data.rows[0].doc)
-			})
+			const persit = await fetch(`${config.couchdbUrl}/nupedidos/${data.rows[0].doc._id}`,
+				{
+					method: 'PUT',
+					headers:{
+						'Authorization': `Basic ${auth}`,
+						'Content-type': 'application/json',
+					},
+					body: JSON.stringify(data.rows[0].doc)
+				})
 
-	}catch(err){
-		console.error(err)
-	}
+		}catch(err){
+			console.error(err)
+		}
+	};
+	
+	async inserirPedidoCouchDb(...args){};
+
 };
